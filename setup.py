d4a5470186093ab82dfd0331ce34427749ec0a87
Nicolas Carmont <nickcarmont8@gmail.com>
2019-11-01 11:05:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'augmently',         # How you named your package folder (MyLib)
-  packages = ['augmently'],   # Chose the same as "name"
+  name = 'Augmently',         # How you named your package folder (MyLib)
+  packages = ['Augmently'],   # Chose the same as "name"
   version = '1.0.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library for Data Augmentation of images in computer vision.',   # Give a short description about your library
