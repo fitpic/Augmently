eb5682b67fd0deef01c9a4c897f714fed24fc941
Nicolas Carmont <nickcarmont8@gmail.com>
2019-11-01 11:19:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Augmently',         # How you named your package folder (MyLib)
   packages = ['Augmently'],   # Chose the same as "name"
-  version = '1.0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library for Data Augmentation of images in computer vision.',   # Give a short description about your library
   author = 'Nicolas Carmont',                   # Type in your name
   author_email = 'nickcarmont8@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/fitpic/Augmently',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/fitpic/Augmently/archive/1.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/fitpic/Augmently/archive/1.0.2.tar.gz',    # I explain this later on
   keywords = ['image', 'augmentation', 'machine learning', 'CNN', 'data augmentation','deep learning','data processing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
